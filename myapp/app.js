29e69f4d6fe59cc4e6bdf2f22f0c0fef84ecc703
SahoryHerrera <sahory0920@gmail.com>
2024-04-24 20:31:22 -0600

    Nodemon implementado y ejecutado

--- myapp/app.js
+++ myapp/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Instalacion y ejecucion de express completado')
+  res.send('Instalacion y ejecucion de nodemon')
 })
 
 app.listen(port, () => {
