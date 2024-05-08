1d2e262a12ddc7804d57b6482491d751d03e030a
SahoryHerrera <sahory0920@gmail.com>
2024-05-07 20:28:51 -0600

    Agregado .env

--- myapp/app.js
+++ myapp/app.js
@@ -1,25 +1,27 @@
+require('dotenv').config();
+
 const express = require('express');
-const axios = require('axios'); // Importa el módulo axios
+const axios = require('axios');
+const inventoryRouter = require('./routes/inventory.js'); // Ruta al enrutador de inventario
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 // Ruta para obtener un usuario aleatorio
 app.get('/random-user', async (req, res) => {
   try {
-    // Realiza una solicitud a la API de Random User Generator
-    const response = await axios.get('https://randomuser.me/api/');
-    
-    // Envía los datos del usuario aleatorio como respuesta
+    const response = await axios.get(process.env.RANDOM_USER_API_URL);
     res.json(response.data);
   } catch (error) {
-    // Manejo de errores en caso de que falle la solicitud
     console.error('Error al obtener usuario aleatorio:', error);
     res.status(500).json({ error: 'Ocurrió un error al obtener el usuario aleatorio' });
   }
 });
+
+// Ruta de inventario
+app.use('/inventory', inventoryRouter);
 
 app.listen(port, () => {
   console.log(`Servidor Express escuchando en el puerto ${port}`);
